bf82f9278baf4c57ced26dffaf83b178aee92d05
caiwenglong <836472117@qq.com>
2022-10-24 17:56:35 +0800

    init

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -46,3 +46,12 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+// vue3 报错提示 找不到模块"./XXX.vue"或其他相应模块
+// 报错原因：typescript 只能理解.ts 文件，无法理解.vue 文件
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
